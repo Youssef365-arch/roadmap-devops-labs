3a8c320b09a7a6a4fb9c52da2bfcc7df9f36eb6e
youssef <youssefmelk03@gmail.com>
2025-02-26 14:16:53 -0500

    CPU usage

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -2,4 +2,5 @@ uptime
 
 echo
 
-top -bn1
+# Print Cpu usage from top command, specifically by subtracting idle time from 100 to get usage
+top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{ print "CPU Usage: " 100-$1 "%" " (Idle: " $1 "%)" }'
