8cedea865ad76a4c97b2d6797c8c6975eb7cb161
youssef <youssefmelk03@gmail.com>
2025-02-28 23:56:03 -0500

    added top 5 processes by cpu and mem usage

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -15,3 +15,13 @@ echo
 
 # Disk Usage (Free vs Used including percentage)
 df | grep C: | awk {' printf "Disk Used: %.2f GiB (%.2f%%)\nDisk Free: %.2f GiB (%.2f%%)\n", $3/(1024^2), ($3/$2)*100, $4/(1024^2), ($4/$2)*100 '}
+
+echo
+
+# Top 5 processes by CPU usage
+ps aux --sort -%cpu | head -n6 | awk '{print $1 "\t" $2 "\t" $3 "\t" $11}'
+
+echo
+
+# Top 5 processes by memory usage
+ps aux --sort -%mem | head -n6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
