73173815ec3f2cfa2474f664d7d7e58eb115f4d3
youssef <youssefmelk03@gmail.com>
2025-03-07 12:38:21 -0500

    completed nginx-log-analyser.sh and README.md

---
+++ nginx-log-analyser/README.md
@@ -0,0 +1,24 @@
+# Nginx Log Analyser
+Write a simple tool to analyze logs from the command line.
+
+# 1. Clone the Repository
+```
+git clone https://github.com/Youssef365-arch/roadmap-devops-labs.git
+cd nginx-log-analyser
+```
+
+# 2. Ensure the Script File is Executable
+```
+chmod +x nginx-log-analyser.sh
+```
+*or*
+```
+chmod 744 nginx-log-analyser.sh
+```
+
+# 3. Execute the Script
+```
+./nginx-log-analyser.sh </path/to/log-file>
+```
+
+[This project](https://roadmap.sh/projects/nginx-log-analyser) is thanks to roadmap.sh [DevOps projects](https://roadmap.sh/devops/projects).
--- nginx-log-analyser/nginx-log-analyser.sh
+++ nginx-log-analyser/nginx-log-analyser.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
-: '
-
-The goal of this project is to help you practice some basic shell scripting skills.
-You will write a simple tool to analyze logs from the command line.
-
-The log file contains the following fields:
-
-IP address
-Date and time
-Request method and path
-Response status code
-Response size
-Referrer
-User agent
-
-create a shell script that reads the log file and provides the following information:
-
-Top 5 IP addresses with the most requests
-    - look for the 5 highest requests, THEN extract the IP out of those lines.
-Top 5 most requested paths
-    - look for the 5 highest requested paths THEN extract the paths.
-Top 5 response status codes
-    -  look for the 5 highest response status codes THEN extract the codes
-Top 5 user agents
-    - look for the 5 highest user agents THEN extract the user agents
-
-'
 
 main() {
     # pass entire cmd line arg into validate_input()
     validate_input $@
+
     # Top 5 IP addresses with the most requests
-    awk '{print $1}' $1 | sort | uniq -c | sort -r | head -n5 | awk '{printf "Top 5 IP addresses: %s (%d Requests)\n", $2, $1}'    
+    echo Top 5 IP addresses: 
+    awk '{print $1}' $1 | sort | uniq -c | sort -r | head -n5 | awk '{printf "%s (%d Requests)\n", $2, $1}'
+    
+    echo
+    
+    # Top 5 most requested paths
+    echo Top 5 most requested paths: 
+    awk '{print $7}' $1 | sort | uniq -c | sort -r | head -n5 | awk '{printf "%s (%d Requests)\n", $2, $1}'
+
+    echo
+
+    # Top 5 response status codes (print response status codes between 100-599)
+    echo Top 5 response status codes: 
+    grep -oE ' [1-5][0-9]{2} ' $1 | sort | uniq -c | sort -r | head -n5 | awk '{printf "%s (%d Requests)\n", $2, $1}'
+
+    echo
+
+    # Top 5 user agents
+    echo Top 5 user agents
+    awk -F'"' '{print $6}' $1 | sort | uniq -c | sort -r | head -n5 | awk '{printf "%s (%d Requests)\n", substr($0, index($0, $2)), $1}'
 }
 
 validate_input() {
     # user provided no cmd line args
     if [ -z "$1" ]; then
         echo -e "[ERROR] Must provide a log file\n\tUsage: ./nginx-log-analyser </path/to/log-file>"
+        exit 1
+    # user provided more than 1 cmd line arg
+    elif [ $# -ne 1 ]; then
+        echo -e "[ERROR] Must provide only one argument\n\tUsage: ./nginx-log-analyser </path/to/log-file>"
+        exit 1
+    # argument/file provided by user cannot be found
+    elif [ ! -f "$1" ]; then
+        echo -e "[ERROR] '"$1"' not found\n\tUsage: ./nginx-log-analyser </path/to/log-file>"
         exit 1
     # user provided a directory
     elif [ -d "$1" ]; then
